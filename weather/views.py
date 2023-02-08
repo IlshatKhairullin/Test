7375db3d897beb3c9fb5c2d320658c07be36511b
ilshatkhairullin <ilshat1941@yandex.ru>
2023-02-08 14:55:33 +0300

    + main page draft with 2 buttons

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,19 @@
+import requests
+import os
 from django.shortcuts import render
+from dotenv import load_dotenv, find_dotenv
 
-# Create your views here.
+load_dotenv(find_dotenv())
+open_weather_token = os.environ.get('OPEN_WEATHER_TOKEN', '')
+
+
+def main_page(request):
+    return render(request, 'weather/main_page.html', {})
+
+
+def weather_now(request):
+    pass
+
+
+def weather_forecast(request):
+    pass
