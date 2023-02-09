3281a6d9a6ffb4d32981d5427602c32b92d8df01
ilshatkhairullin <ilshat1941@yandex.ru>
2023-02-09 17:14:54 +0300

    + main page test, search test

--- weather/tests.py
+++ weather/tests.py
@@ -1,3 +1,34 @@
+from http import HTTPStatus
 from django.test import TestCase
+from django.urls import reverse
+from requests import Response
 
-# Create your tests here.
+
+class MainPageTestCase(TestCase):
+    def _check_response(self, page: str, equal: bool = True, query_params: dict = None) -> Response:
+        response = self.client.get(reverse(page), data=query_params)
+        if equal:
+            self.assertEqual(response.status_code, HTTPStatus.OK)
+        else:
+            self.assertNotEqual(response.status_code, HTTPStatus.OK)
+        return response
+
+    def test_main_page(self):
+        response = self._check_response(page='main_page')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_weather_now_with_search(self):
+        response = self._check_response(page='weather_now', query_params={"search": "Казань"})
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_weather_now_with_search_empty(self):
+        response = self._check_response(page='weather_now', equal=False, query_params={"search": ""})
+        self.assertNotEqual(response.status_code, HTTPStatus.OK)
+
+    def test_weather_forecast_with_search(self):
+        response = self._check_response(page='weather_forecast', query_params={"search": "Kazan"})
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_weather_forecast_with_search_empty(self):
+        response = self._check_response(page='weather_forecast', equal=False, query_params={"search": ""})
+        self.assertNotEqual(response.status_code, HTTPStatus.OK)
