7555f014279efb8e4447c17f3b849eda3a7a7ec7
ilshatkhairullin <ilshat1941@yandex.ru>
2023-02-09 16:30:16 +0300

    + fill data method in dataclass

--- weather/dataclasses.py
+++ weather/dataclasses.py
@@ -1,25 +1,41 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
 class WeatherNow:
     """Датакласс для хранения информации о погоде на данный момент"""
 
-    country: str
-    city: str
-    local_time: str
-    current_temp_c: int
-    feels_like_temp_c: int
-    humidity: int
-    wind_kph: float
-    wind_direction: str
-    icon_url: str
+    country: str = field(init=False)
+    city: str = field(init=False)
+    local_time: str = field(init=False)
+    current_temp_c: int = field(init=False)
+    feels_like_temp_c: int = field(init=False)
+    humidity: int = field(init=False)
+    wind_kph: float = field(init=False)
+    wind_direction: str = field(init=False)
+    icon_url: str = field(init=False)
+
+    def fill_data(self, data: dict) -> None:
+        self.country = data.get("country")
+        self.city = data.get("location")
+        self.local_time = data.get("local_time")
+        self.current_temp_c = data.get("temp_c")
+        self.feels_like_temp_c = data.get("feels_like_c")
+        self.humidity = data.get("humidity")
+        self.wind_kph = data.get("wind_kph")
+        self.wind_direction = data.get("wind_direction")
+        self.icon_url = data.get("icon_url")
 
 
 @dataclass
 class WeatherForecast:
     """Датакласс для хранения информации о прогнозе погоды на 10 дней"""
 
-    country: str
-    city: str
-    forecast: list
+    country: str = field(init=False)
+    city: str = field(init=False)
+    forecast: list = field(init=False)
+
+    def fill_data(self, data: dict) -> None:
+        self.country = data.get("country")
+        self.city = data.get("location")
+        self.forecast = data.get("forecast")
--- weather/services.py
+++ weather/services.py
@@ -47,26 +47,14 @@ def fetch_weather_data(request: HttpRequest, weather_choice: Weather) -> dict:
     location = request.GET.get("search")
 
     if weather_choice == Weather.Now:
-        data = process_weather_now(location=location)
-        weather_now_dataclass = WeatherNow(
-            country=data.get("country"),
-            city=data.get("location"),
-            local_time=data.get("local_time"),
-            current_temp_c=data.get("temp_c"),
-            feels_like_temp_c=data.get("feels_like_c"),
-            humidity=data.get("humidity"),
-            wind_kph=data.get("wind_kph"),
-            wind_direction=data.get("wind_direction"),
-            icon_url=data.get("icon_url"),
-        )
+        data: dict = process_weather_now(location=location)
+        weather_now_dataclass = WeatherNow()
+        weather_now_dataclass.fill_data(data=data)
         context.update(asdict(weather_now_dataclass))
 
     elif weather_choice == Weather.Forecast:
-        data = process_weather_forecast(location=location)
-        weather_forecast_dataclass = WeatherForecast(
-            country=data.get("country"),
-            city=data.get("location"),
-            forecast=data.get("forecast"),
-        )
+        data: dict = process_weather_forecast(location=location)
+        weather_forecast_dataclass = WeatherForecast()
+        weather_forecast_dataclass.fill_data(data=data)
         context.update(asdict(weather_forecast_dataclass))
     return context
